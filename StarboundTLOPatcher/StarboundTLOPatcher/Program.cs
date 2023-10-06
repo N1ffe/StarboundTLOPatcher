c767a80a7951c809b1403440d6be516382b60af1
Niffe <petrovnik01@ukr.net>
2023-10-06 19:47:41 +0300

    Release

---
+++ StarboundTLOPatcher/StarboundTLOPatcher/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StarboundTLOPatcher
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            if (args.Length >= 2)
+            {
+                await TLOPatcher.Run(args[0], args[1]);
+            }
+            else
+            {
+                Console.WriteLine("Program requires 2 parameters:" +
+                    "\n1 - Full path to the target mod object folder, i.e. \"C:\\ModName\\objects\"" +
+                    "\n2 - Full path to your mod folder, i.e. \"C:\\YourMod\"");
+                Console.WriteLine("Example: StarboundTLOPatcher.exe \"C:\\ModName\\objects\" \"C:\\YourMod\"");
+            }
+        }
+    }
+}
---
+++ StarboundTLOPatcher/StarboundTLOPatcher/TLOPatcher.cs
@@ -0,0 +1,70 @@
+﻿namespace StarboundTLOPatcher
+{
+    public static class TLOPatcher
+    {
+        public static async Task Run(string modPath, string outputPath)
+        {
+            try
+            {
+                string patchData = await File.ReadAllTextAsync(Path.Combine(Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName, "patch.json"));
+                string[] objects = Directory.GetFiles(modPath, "*.object", SearchOption.AllDirectories);
+                int count = 0;
+
+                foreach (string obj in objects)
+                {
+                    if (await RequiresPatchAsync(obj))
+                    {
+                        string objPath = obj[obj.IndexOf("object")..obj.LastIndexOf('\\')];
+
+                        Directory.CreateDirectory(Path.Combine(outputPath, objPath));
+                        await File.WriteAllTextAsync(Path.Combine(outputPath, objPath, Path.GetFileName(obj) + ".patch"), patchData);
+
+                        count++;
+                    }
+                }
+
+                Console.WriteLine($"Successfully created {count} patch files.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static async Task<bool> RequiresPatchAsync(string path)
+        {
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string? line;
+
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    // checks work only if key and value are on the same lines
+                    if (line.Contains("\"lightColor\"") || line.Contains("\"activeLightColor\""))
+                    {
+                        if (line.Replace(" ", "").Contains("[0,0,0]")) // check if light color is not 0, 0, 0
+                        {
+                            return false;
+                        }
+
+                        using (StreamReader pointLightReader = new StreamReader(path)) // check if point light is already present
+                        {
+                            string? pointLightCheck;
+
+                            while ((pointLightCheck = await pointLightReader.ReadLineAsync()) != null)
+                            {
+                                if (pointLightCheck.Contains("pointLight") && pointLightCheck.Contains("true"))
+                                {
+                                    return false;
+                                }
+                            }
+                        }
+
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+    }
+}
